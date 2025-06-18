42437db544511bf06dec1483cad7fd56462358dd
rasha <fahimrsha3@gmail.com>
2025-06-18 14:16:18 +0500

    accordian

--- small-projects-react/src/App.jsx
+++ small-projects-react/src/App.jsx
@@ -2,14 +2,15 @@
 
 import './App.css'
 import Accordian from './components/accordian'
-
+import RandomColor from './components/random-color'
 function App() {
 
 
   return (
     <>
       
-       <Accordian/>
+      {/* <Accordian/>*/}
+<RandomColor/>
     </>
   )
 }
