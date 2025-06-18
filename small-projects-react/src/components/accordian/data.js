59354819f6063f58c2cbc5830a1e9192a53a25fd
rasha <fahimrsha3@gmail.com>
2025-06-18 13:19:21 +0500

    accordian

---
+++ small-projects-react/src/components/accordian/data.js
@@ -0,0 +1,17 @@
+const data=[
+{
+    id:'1',
+    question:"what are accordian components?1",
+    answer:"Accordian components are user interface elements used for organ."
+},
+{
+    id:'2',
+    question:"what are accordian components?2",
+    answer:"Accordian components are user interface elements used for organ."
+},{
+    id:'3',
+    question:"what are accordian components?3",
+    answer:"Accordian components are user interface elements used for organ."
+},
+];
+export default data;
