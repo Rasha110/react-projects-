4d6fe5bd1bbe171465b07d38d661d456e2b3acf8
rasha <fahimrsha3@gmail.com>
2025-06-20 21:34:44 +0500

    Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-
-
 import './App.css'
 import Accordian from './components/accordian'
+import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-button'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
+import TreeView from './components/tree-view'
+import menus from "./components/tree-view/data";
+
 function App() {
 
 
   return (
     <>
       
-      {/* <Accordian/>*/}
+      <Accordian/>
 
-{/*<RandomColor/>*/}
+<RandomColor/>
 <StarRating/>
+<ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={5}/>
+<LoadMoreData/>
+<TreeView menus={menus}/>
     </>
   )
 }
